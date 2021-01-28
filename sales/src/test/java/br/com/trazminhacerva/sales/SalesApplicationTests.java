88b0d6eb4514876a48f7531d59def6ae6883d3ae
Marco Prado <marco.prado@gsw.com.br>
2021-01-27 22:12:04 -0300

    Initial project setup

---
+++ sales/src/test/java/br/com/trazminhacerva/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trazminhacerva.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
